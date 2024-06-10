430abbb4c91d2187ec06b65effcfecc9c677da22
T.Sanyi <s.truczkai@gmail.com>
2024-06-10 18:11:05 +0200

    replace Document interface with an abstract class

--- src/main/java/com/github/trusandor/html_builder/Main.java
+++ src/main/java/com/github/trusandor/html_builder/Main.java
@@ -2,6 +2,5 @@ package com.github.trusandor.html_builder;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/com/github/trusandor/html_builder/abstractClasses/Document.java
@@ -0,0 +1,7 @@
+package com.github.trusandor.html_builder.abstractClasses;
+
+public abstract class Document {
+    protected String docType;
+
+    public abstract String toString();
+}
